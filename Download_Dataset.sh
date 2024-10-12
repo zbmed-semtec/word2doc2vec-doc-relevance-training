cc17122f1355ec58ea9047d85b5f51abc52e407a
Rohitha Ravinder <75835121+rohitharavinder@users.noreply.github.com>
2024-10-12 20:17:23 +0200

    🐛 update Download_Dataset.sh

--- Download_Dataset.sh
+++ Download_Dataset.sh
@@ -2,14 +2,14 @@ sudo pip install gdown
 cd data
 
 mkdir -p Split_Dataset/Data
-mkdir -p Split_Dataset/Groundtruth
+mkdir -p Split_Dataset/Ground_truth
 
 cd Split_Dataset/Data/
 gdown  https://drive.google.com/uc?id=1IgxzmajC09aUTz_awABPD_iEJ2s1VR0E -O test.npy
 gdown  https://drive.google.com/uc?id=1xCoXFl0GTc5VbW7L-7joAL9ViudD0Nbh -O train.npy
 gdown https://drive.google.com/uc?id=1De4n5hf6kirLpMlILD2LTFTuwsgpVhKQ -O valid.npy
 
-cd ../Groundtruth
+cd ../Ground_truth
 gdown  https://drive.google.com/uc?id=1y9T41Faf9Oq2XOtWMD1U9fZe9OHLgLjv -O test.tsv
 gdown  https://drive.google.com/uc?id=1R1i74XWzILnlozwCfYItlequKIhMnHmB -O train.tsv
 gdown https://drive.google.com/uc?id=1ZupxAdTOWxmKPWlD5FOwEKbkdavt5Zxk -O valid.tsv
